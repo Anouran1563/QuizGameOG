db57106100bc935ed989cdac21f53df83771eb9f
Anouar Zinedine <anouarzinedine16@gmail.com>
2023-12-15 19:58:22 +0100

    Projektdateien hinzufügen.

---
+++ QuizGameOG/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace QuizGameOG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft New Tai Lue", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(14, 266);
+            this.lblQuestion.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(103, 42);
+            this.lblQuestion.TabIndex = 16;
+            this.lblQuestion.Text = "label1";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuestion.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(578, 476);
+            this.btn4.Margin = new System.Windows.Forms.Padding(5);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(506, 140);
+            this.btn4.TabIndex = 15;
+            this.btn4.Text = "button1";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(14, 493);
+            this.btn3.Margin = new System.Windows.Forms.Padding(5);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(475, 140);
+            this.btn3.TabIndex = 14;
+            this.btn3.Text = "button1";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(578, 313);
+            this.btn2.Margin = new System.Windows.Forms.Padding(5);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(506, 129);
+            this.btn2.TabIndex = 12;
+            this.btn2.Text = "button1";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(14, 313);
+            this.btn1.Margin = new System.Windows.Forms.Padding(5);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(475, 129);
+            this.btn1.TabIndex = 13;
+            this.btn1.Text = "button1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-129, -97);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1233, 358);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 42);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(571, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 42);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 493);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 42);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(571, 476);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 42);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "D";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 42);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 647);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ QuizGameOG/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizGameOG
+{
+    public partial class Form1 : Form
+    {
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+        public Form1()
+        {
+            InitializeComponent();
+            askQuestion(questionNumber);
+            totalQuestions = 5;
+        }
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+            int btnTag = Convert.ToInt32(senderObject.Tag);
+            if (btnTag == correctAnswer)
+            {
+                score++;
+            }
+            if (questionNumber == totalQuestions)
+            {
+                // work out the percentage
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+                MessageBox.Show(
+                    "Quiz ist Zuende!" + Environment.NewLine +
+                    "Du hast " + score + " Fragen richtig beantwortet." + Environment.NewLine +
+                    "Deine Prozentzahl " + percentage + "%" + Environment.NewLine +
+                    "Clicke OK um die Lösungen zu sehen"
+                    );
+                MessageBox.Show("Wer malte die MonaLisa? = B Leonardo Davinci" + Environment.NewLine +
+                    "Was ist das seltenste Mineral? = C Kyawthuit" + Environment.NewLine +
+                    "Was kam Zuerst? Huhn oder Ei = A Ei" + Environment.NewLine +
+                    "Wer ist der beste Youtuber? = A Mrbeast" + Environment.NewLine +
+                    "Was ist das seltenste Material im Universum aus den unteren Antworten? = D Holz");
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+            }
+            questionNumber++;
+            askQuestion(questionNumber);
+        }
+        private void askQuestion(int qnum)
+        {
+            switch (qnum)
+            {
+                case 1:
+                    lblQuestion.Text = "Wer malte die MonaLisa?";
+                    btn1.Text = "Leonardo Davinchi";
+                    btn2.Text = "Leonardo Davinci";
+                    btn3.Text = "Vincent van Gogh";
+                    btn4.Text = "Kevin Hart";
+                    correctAnswer = 2;
+                    break;
+                case 2:
+                    lblQuestion.Text = "Was ist das seltenste Mineral?";
+                    btn1.Text = "Citrin";
+                    btn2.Text = "Amethyst";
+                    btn3.Text = "Kyawthuit";
+                    btn4.Text = "Obsidian";
+                    correctAnswer = 3;
+                    break;
+                case 3:
+                    lblQuestion.Text = "Was kam Zuerst? Huhn oder Ei";
+                    btn1.Text = "Ei";
+                    btn2.Text = "Huhn";
+                    btn3.Visible = false;
+                    label3.Visible = false;
+                    btn4.Visible = false;
+                    label4.Visible = false;
+                    correctAnswer = 1;
+                    break;
+
+                case 4:
+                    btn3.Visible = true;
+                    label3.Visible = true;
+                    btn4.Visible = true;
+                    label4.Visible = true;
+                    lblQuestion.Text = "Welche Buchstaben werden in der Geometrie verwendet?";
+                    btn1.Text = "Deutsche";
+                    btn2.Text = "Griechische";
+                    btn3.Text = "Englische";
+                    btn4.Text = "Französiche";
+                    correctAnswer = 2;
+                    break;
+                case 5:
+                    lblQuestion.Text = "Was ist das seltenste Material im Universum aus den unteren Antworten?";
+                    btn1.Text = "Platinum";
+                    btn2.Text = "Gold";
+                    btn3.Text = "Nullitin";
+                    btn4.Text = "Holz";
+                    correctAnswer = 4;
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
